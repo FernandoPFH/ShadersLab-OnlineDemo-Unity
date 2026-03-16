0c6f6eb3cf16b39ace50cfbc245bb58ea1fb0c1c
FernandoPFH <51247535+FernandoPFH@users.noreply.github.com>
2026-03-16 01:32:52 -0300

    Feat: Adicionado shader "Pixelizer Post Processing"
    - Adicionado shader
    - Melhorado sistema para customização por ShaderInfos
    - Criado sistema para gerar UI por ShaderInfos

---
+++ ShadersLab/Assets/MenuInicial/Scripts/ScripableObjects/Custom/PixelizerShaderInfos.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using NaughtyAttributes;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PixelizerShaderInfos", menuName = "ScriptableObjects/Custom/PixelizerShaderInfos")]
+public class PixelizerShaderInfos : ShaderInfos
+{
+    public Material StencilMaterial;
+
+    protected override void GenerateShaderUI()
+    {
+        (SceneSetupHandler as SetupPixelizerHandler).GenerateUI();
+
+        base.GenerateShaderUI();
+    }
+
+    public override void OnSceneStart()
+    {
+        base.OnSceneStart();
+
+        (SceneSetupHandler as SetupPixelizerHandler).SetupNewMaterial(Material, StencilMaterial);
+    }
+}
